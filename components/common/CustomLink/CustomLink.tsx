c62427aa322076cf03769a96d08009f22db38fe3
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-15 20:48:16 +0800

    Update next link to use its legacyBehavior

--- components/common/CustomLink/CustomLink.tsx
+++ components/common/CustomLink/CustomLink.tsx
@@ -1,19 +1,18 @@
 import NextLink from 'next/link';
-import { Link, LinkProps } from '@chakra-ui/react';
+import { Link as CharkaLink, LinkProps } from '@chakra-ui/react';
 
 export interface ICustomLinkProps extends LinkProps {
   href: string;
-  title: string;
   external?: boolean;
 }
 
 const CustomLink = (props: ICustomLinkProps) => {
-  const { href, title, color = 'teal.400', ...rest } = props;
+  const { href, children, color = 'teal.400', ...rest } = props;
   return (
-    <NextLink href={href} passHref>
-      <Link color={color} {...rest}>
-        {title}
-      </Link>
+    <NextLink href={href} passHref legacyBehavior>
+      <CharkaLink color={color} {...rest}>
+        {children}
+      </CharkaLink>
     </NextLink>
   );
 };
