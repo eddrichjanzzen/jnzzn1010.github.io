3ae9bf3ac6e5d769b48d1e75f0c110adf64367d4
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-21 22:28:09 +0800

    Customize fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from 'next/app';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import MainLayout from '../components/layout/MainLayout';
+import theme from '../theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
