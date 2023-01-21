831977c28467b076ff24a5bec86792f0d91d3b9b
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-21 22:08:41 +0800

    Clean unused vars

--- pages/posts/[postSlug].tsx
+++ pages/posts/[postSlug].tsx
@@ -2,7 +2,6 @@ import { Box, Heading } from '@chakra-ui/react';
 import PostBody from '../../components/Post/PostBody';
 
 import Post from '../../interfaces/Post.interface';
-import markdownToHtml from '../../lib/markdownToHtml';
 import { getAllPosts, getPostBySlug } from '../../lib/posts.api';
 
 export interface IPostDetailPageParams {
@@ -12,7 +11,7 @@ export interface IPostDetailPageParams {
 const PostDetailPage = ({ post }: IPostDetailPageParams) => {
   return (
     <Box>
-      <Heading>{post.title}</Heading>
+      <Heading mb={8}>{post.title}</Heading>
       <PostBody content={post.content} />
     </Box>
   );
@@ -26,8 +25,6 @@ type Params = {
 
 export async function getStaticProps({ params }: Params) {
   const post = getPostBySlug(params.postSlug, ['title', 'content']);
-
-  // const content = await markdownToHtml(post.content || '');
 
   return {
     props: {
