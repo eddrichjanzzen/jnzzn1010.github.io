44d337a1ba0b02e36e1a5eb62cf2c3458ddb0375
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-28 20:20:50 +0800

    Implement with react-syntax-highlighter

--- components/common/Markdown/Markdown.tsx
+++ components/common/Markdown/Markdown.tsx
@@ -1,8 +1,8 @@
 import ReactMarkdown from 'react-markdown';
-import CodeBlock from '../CodeBlock/CodeBlock';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import remarkGemoji from 'remark-gemoji';
 import remarkGfm from 'remark-gfm';
+import MarkdownConfig from './Markdown.config';
 
 interface IMarkdownProps {
   markdown: string;
@@ -12,7 +12,7 @@ const Markdown = ({ markdown }: IMarkdownProps) => {
   return (
     <ReactMarkdown
       children={markdown}
-      components={ChakraUIRenderer()}
+      components={ChakraUIRenderer(MarkdownConfig)}
       remarkPlugins={[remarkGfm, remarkGemoji]}
     />
   );
