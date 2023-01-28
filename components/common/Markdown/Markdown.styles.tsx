e2795de006f37f6b5efcd5573d22dc5b8122dd42
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-28 22:24:27 +0800

    Simplify markdown configs

---
+++ components/common/Markdown/Markdown.styles.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const MarkdownContainer = styled.div`
+  border: 1px solid grey;
+  padding: 1px;
+  border-radius: 0.3em;
+  margin: 1em 0;
+`;
+
+export default MarkdownContainer;
