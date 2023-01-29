82697196aa7fbcda729b6a08391bf44d1e938694
Eddrich Janzzen <eddrichang@gmail.com>
2023-01-29 12:00:33 +0800

    Add remark-emoji

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,10 +1,12 @@
 import { remark } from 'remark';
 import html from 'remark-html';
 import prism from 'remark-prism';
+import emoji from 'remark-emoji';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(prism)
+    .use(emoji)
     .use(html, { sanitize: false })
     .process(markdown);
   return result.toString();
