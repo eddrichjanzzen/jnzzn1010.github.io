badff10259bca268efc0d9061a9f56b7e6e2d0f6
Eddrich Janzzen <eddrichang@gmail.com>
2023-02-08 23:42:37 +0800

    Add formspree package

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -8,49 +8,81 @@ import {
   Textarea,
   Grid,
   GridItem,
-  Center,
 } from '@chakra-ui/react';
+import { useForm } from '@formspree/react';
+import MailSuccess from '../../components/contact/MailSuccess';
 
 const ContactPage = () => {
+  const [state, handleSubmit] = useForm('myrezndx');
+  const isError = state.errors.length > 1;
+
+  if (state.succeeded) {
+    return <MailSuccess />;
+  }
   return (
     <Box>
       <Heading>Contact</Heading>
-      <Box pt={4}>
-        <Grid
-          templateAreas={`
+      <form method="POST" onSubmit={handleSubmit}>
+        <Box pt={4}>
+          <Grid
+            templateAreas={`
                   "name email"
                   "message message"
                   "button button"
                   `}
-          gridTemplateRows="0.5fr 1fr 0.5fr"
-          gridTemplateColumns="repeat(2, 1fr)"
-          gap="4"
-        >
-          <GridItem area="name" h="100%">
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <Input type="text" placeholder="John Doe" />
-            </FormControl>
-          </GridItem>
-          <GridItem area="email">
-            <FormControl>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" placeholder="test@mailbox.com" />
-            </FormControl>
-          </GridItem>
-          <GridItem area="message">
-            <FormControl>
-              <FormLabel>Message</FormLabel>
-              <Textarea placeholder="Hi Janzzen..." />
-            </FormControl>
-          </GridItem>
-          <GridItem area="button">
-            <Button mt={4} colorScheme="teal" type="submit">
-              Submit
-            </Button>
-          </GridItem>
-        </Grid>
-      </Box>
+            gridTemplateRows="0.5fr 1fr 0.5fr"
+            gridTemplateColumns="repeat(2, 1fr)"
+            gap="4"
+          >
+            <GridItem area="name" h="100%">
+              <FormControl isInvalid={isError}>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="John Doe"
+                  id="name"
+                  name="name"
+                  isRequired
+                />
+              </FormControl>
+            </GridItem>
+            <GridItem area="email">
+              <FormControl isInvalid={isError}>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="test@mailbox.com"
+                  id="email"
+                  name="email"
+                  isRequired
+                />
+              </FormControl>
+            </GridItem>
+            <GridItem area="message">
+              <FormControl isInvalid={isError}>
+                <FormLabel>Message</FormLabel>
+                <Textarea
+                  placeholder="Hi Janzzen..."
+                  id="message"
+                  name="message"
+                  isRequired
+                />
+              </FormControl>
+            </GridItem>
+            <GridItem area="button">
+              <Button
+                mt={4}
+                colorScheme="teal"
+                type="submit"
+                disabled={state.submitting}
+                isLoading={state.submitting}
+              >
+                Submit
+              </Button>
+            </GridItem>
+          </Grid>
+        </Box>
+      </form>
     </Box>
   );
 };
