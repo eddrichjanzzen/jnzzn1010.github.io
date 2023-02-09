195674feef00d0b34e5f6e694b3aa35bbc6a7ca9
Eddrich Janzzen <eddrichang@gmail.com>
2023-02-09 17:39:52 +0800

    Update props

--- components/common/CustomLink.tsx
+++ components/common/CustomLink.tsx
@@ -8,9 +8,15 @@ export interface ICustomLinkProps extends LinkProps {
 
 const CustomLink = (props: ICustomLinkProps) => {
   const { href, children, color = 'teal.500' } = props;
+  // typecast to any to prevent issues with three/drei or three/fibre
+  // https://zenn.dev/t4aru/scraps/3fee3b320bf125
+
+  const propsAny = props as any;
   return (
     <NextLink href={href} passHref legacyBehavior>
-      <CharkaLink color={color}>{children}</CharkaLink>
+      <CharkaLink color={color} {...propsAny}>
+        {children}
+      </CharkaLink>
     </NextLink>
   );
 };
