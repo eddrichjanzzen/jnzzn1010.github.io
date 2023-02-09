77b6ca8c50e6a8990702b4e5553e8211c1919aa9
Eddrich Janzzen <eddrichang@gmail.com>
2023-02-09 18:15:31 +0800

    Add GTAG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,34 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import MainLayout from '../components/layout/MainLayout';
 import theme from '../theme/theme';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </ChakraProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.GTAG}, {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ChakraProvider theme={theme}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ChakraProvider>
+    </>
   );
 }
