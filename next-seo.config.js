7faa09d347049da3765b26334eda0dca88d0cd5d
Eddrich Janzzen <eddrichang@gmail.com>
2023-02-12 22:00:02 +0800

    Downgrade nextjs version

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,8 @@
 // next-seo.config.js
 export const NEXT_SEO_DEFAULT = {
-  title: 'Janzzen Personal Blog',
-  description: 'Janzzen Personal blog',
+  title: 'Janzzen | Personal Blog',
+  description:
+    'This is my personal blog. Where I blog about tech, my life and others',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
