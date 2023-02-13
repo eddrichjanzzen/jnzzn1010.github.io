ba8ecca75d09fc85f61872585277c33e1967da4d
Eddrich Janzzen <eddrichang@gmail.com>
2023-02-13 17:14:43 +0800

    Add coming soon to pages

---
+++ pages/coding-problems/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const CodingProblemsPage = () => {
+  return (
+    <Box>
+      <Heading>Coming Soon!</Heading>
+    </Box>
+  );
+};
+
+export default CodingProblemsPage;
---
+++ pages/personal-development/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const PersonalDevelopmentPage = () => {
+  return (
+    <Box>
+      <Heading>Coming Soon!</Heading>
+    </Box>
+  );
+};
+
+export default PersonalDevelopmentPage;
